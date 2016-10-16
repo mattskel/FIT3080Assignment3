426bfd9ea056cefcf75ddaedf8f4fa0400528bad
Matthew Skelley <matthewskelley@dyn-118-138-26-241.its.monash.edu.au>
2016-10-17 09:21:08 +1100

    Commit Monday 17 October 9:20AM

--- question2.py
+++ question2.py
@@ -1,5 +1,6 @@
 import math
 import numpy as np
+import random
 
 def read_datafile(fname, attribute_data_type = 'integer'):
    inf = open(fname,'r')
@@ -32,41 +33,52 @@ class DecisionTree :
       self.A = None
       self.branch = []
       self.label = default_label
-      
+      self.leaf = None
       
 	
    def train(self, X_train, Y_train) :
       # receives a list of objects of type Example
       # TODO: implement decision tree training
-      
+      # Set label to the plurality value
+      self.label = pluralityValue(Y_train)
       # Check i X_train is empty
       if len(X_train) is 0:
+         # When we are searching in testing, if we get back none, should do plurality value
          return None
       elif all(y==Y_train[0] for y in Y_train):
          # TODO: return the classification
-         return Y_train[0]
-         None
+         # should still return a tree here
+         # just need to acknowledge it is a leaf
+         # could look at the branch to see if it is empty or check if label has a value
+         self.leaf = Y_train[0]
+         return self
+      elif self.curr_depth == self.depth_limit:
+         self.leaf = pluralityValue(Y_train)
+         return self
       else:
          # Check entropy of all attributes
          # We don't need to remove the attributes already used
          # Using an attribute already used will result in 0 information gain
-         self.A = argMax(len(X_train[0]),x,y)
+         self.A = argMax(len(X_train[0]),X_train,Y_train)
          for v_k in range(2):
             X_exs = [e for e in X_train if e[self.A] == v_k]
-            print X_exs
-            Y_exs = [y[i] for i in range(len(y)) if x[i][self.A] == v_k]
-            print Y_exs
+            #print X_exs
+            Y_exs = [Y_train[i] for i in range(len(Y_train)) if X_train[i][self.A] == v_k]
+            #print Y_exs
             subtree = DecisionTree(self.split_random,self.depth_limit,self.curr_depth+1,v_k)
             self.branch.append(subtree.train(X_exs,Y_exs))
-            print "HERE"
          return self
-         None
       None
          
    def predict(self, X_train):
       # receives a list of booleans
       # TODO: implement decision tree prediction
-      
+      if self.leaf is not None:
+         return self.leaf
+      elif self.branch[X_train[self.A]] is not None:
+         return self.branch[X_train[self.A]].predict(X_train)
+      else:
+         return self.label
       None
 
 #===
@@ -90,35 +102,25 @@ def argMax(attributes,x,y):
    maxIndex = -1
    max = 0
    for a in range(attributes):
-      None
-      print s_v(x,y,0,0)
-      infoGain = entropy(y) - np.sum([0,1,1])
+      #print s_v(x,y,0,0)
+      #infoGain = entropy(y) - np.sum([0,1,1])
       infoGain = entropy(y) - np.sum([float(len(s_v(x,y,v,a))) / float(len(y)) * entropy(s_v(x,y,v,a)) for v in range(2)])
       if infoGain > max:
-         print infoGain
+         #print infoGain
          maxIndex = a
-   print maxIndex
-   return maxIndex
-
-"""
-#===
-# Pass in attributes
-# For our case this will be an int refering to the total number of attributes
-def argMax(attributes,x,y):
-   maxIndex = -1
-   max = 0
-   for a in range(attributes):
-      infoGain =  entropy(y) - np.sum([float(len(s_v(x,y,v,a))) / float(len(y)) * entropy(s_v(x,y,v,a)) for v in range(2)])
-      if infoGain >= max:
-         maxIndex = a
-         max = infoGain
+   #print maxIndex
    return maxIndex
 
 #===
-
-def s_v(x,y,v,a):
-   return [y[i] for i in range(len(y)) if x[i][a] == v]
-"""
+# Returns the most common output value among a set of examples
+# breaks ties evenly
+def pluralityValue(y):
+   if y.count(0) > y.count(1):
+      return 0
+   elif y.count(1) > y.count(0):
+      return 1
+   else:
+      return random.randint(0, 1)
 
 #===
 def compute_accuracy(dt_classifier, X_test, Y_test):
@@ -136,42 +138,17 @@ X_train, Y_train = read_datafile('train.txt')
 X_test, Y_test = read_datafile('test.txt')
 # TODO: write your code
 
-x = [[0,0,0],[1,1,1],[0,1,0],[0,0,1]]
-y = [0,1,1,0]
-a = 0
-print x
-print y
+#x = [[0,0,0],[1,1,1],[0,1,0],[0,0,1]]
+#y = [0,1,1,0]
+#a = 0
 
-argMax(3,x,y)
+#argMax(3,x,y)
 
-dt = DecisionTree(0,0,0,1)
-dt.train(x,y)
-
-
-"""
-   sum = 0
-   for v in range(2):
-   s_v = [y[i] for i in range(len(y)) if x[i][a] == v]
-   print s_v
-   print entropy(s_v)
-   print float(len(s_v)) / float(len(y)) * entropy(s_v)
-   sum += float(len(s_v)) / float(len(y)) * entropy(s_v)
-   print sum
-   """
-
-"""
-   def s_v(x,y,v,a):
-   return [y[i] for i in range(len(y)) if x[i][a] == v]
-   
-   def argMax(attributes,x,y):
-   maxIndex = -1
-   max = 0
-   for a in range(attributes):
-   infoGain = entropy(y) - np.sum([float(len(s_v(x,y,v,a))) / float(len(y)) * entropy(s_v(x,y,v,a)) for v in range(2)])
-   if infoGain > max:
-   print infoGain
-   maxIndex = a
-   print maxIndex
-   return maxIndex
-   """
+depthLimit = 4
+splitFlag = 0
+dt = DecisionTree(splitFlag,depthLimit,0,1)
+#dt.train(x,y)
+dt.train(X_train,Y_train)
+#print dt.predict([1,0,0])
+print compute_accuracy(dt,X_test,Y_test)
 
